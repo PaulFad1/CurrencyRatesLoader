b0c6ef3a7f9420bf835c1ea36cd1aadd1f341299
PaulFad <paul_fad@icloud.com>
2023-10-27 14:03:34 +0300

    Load data for last month

--- CBRProvider.cs
+++ CBRProvider.cs
@@ -6,18 +6,36 @@ public class CBRProvider
 {
     private HttpClient httpClient = new HttpClient() { BaseAddress = new Uri("https://cbr.ru/scripts/") };
     private XmlSerializer serializer;
+
+    public string Check(int num) 
+    {
+        if(num < 10)
+        {
+            return "0" + num;
+        }
+        else
+        {
+            return num.ToString();
+        }
+    }
     public CBRProvider()
     {
         Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
         serializer = new XmlSerializer(typeof(ValCurs));
     }
 
-    public async Task<ValCurs> GetData()
+    public async Task<List<ValCurs>> GetData()
     {
+        List<ValCurs> valCurs = new List<ValCurs>();
         var now = DateTime.Now;
-        
-        var response = await httpClient.GetAsync("XML_daily.asp?date_req=" + now.Day + "/" + now.Month + "/" + now.Year);
-        var res = await response.Content.ReadAsStreamAsync();
-        return (ValCurs)serializer.Deserialize(res);
+        var startDate = now.AddMonths(-1);
+        while (startDate <= now)
+        {
+            var response = await httpClient.GetAsync("XML_daily.asp?date_req=" + Check(startDate.Day) + "/" + Check(startDate.Month) + "/" + startDate.Year);
+            var res = await response.Content.ReadAsStreamAsync(); 
+            valCurs.Add((ValCurs)serializer.Deserialize(res));
+            startDate = startDate.AddDays(1);
+        }
+        return valCurs;
     }
 }
--- DbProvider.cs
+++ DbProvider.cs
@@ -9,27 +9,30 @@ public class DbProvider
         db = new AppContext();
     }
 
-    public async Task LoadToDb(ValCurs valCurs)
+    public async Task LoadToDb(List<ValCurs> valCursList)
     {
-        foreach (var val in valCurs.Valute)
+        foreach (var valCurs in valCursList)
         {
-            var newValute = new ValuteDB() { Id = val.ID, CharCode = val.CharCode, Name = val.Name, NumCode = val.NumCode };
-            if (!db.Valutes.Any(x => x.Id == newValute.Id))
+            foreach (var val in valCurs.Valute)
             {
-                db.Valutes.Add(newValute);
-            }
-            await db.SaveChangesAsync();
-            var datevalues = db.Valutes.Include(x => x.DateValues);
-            if (!datevalues.Where(x => x.Id == newValute.Id).FirstOrDefault().DateValues.Any(x => x.Date == Convert.ToDateTime(valCurs.Date).ToUniversalTime()))
-            {
-                db.Valutes.Where(x => x.Id == newValute.Id).FirstOrDefault().DateValues.Add(new DateValue()
+                var newValute = new ValuteDB() { Id = val.ID, CharCode = val.CharCode, Name = val.Name, NumCode = val.NumCode };
+                if (!db.Valutes.Any(x => x.Id == newValute.Id))
                 {
-                    Date = Convert.ToDateTime(valCurs.Date).ToUniversalTime(),
-                    Nominal = val.Nominal,
-                    Value = Convert.ToDouble(val.Value),
-                    VunitRate = Convert.ToDouble(val.VunitRate)
-                });
+                    db.Valutes.Add(newValute);
+                }
                 await db.SaveChangesAsync();
+                var datevalues = db.Valutes.Include(x => x.DateValues);
+                if (!datevalues.Where(x => x.Id == newValute.Id).FirstOrDefault().DateValues.Any(x => x.Date == Convert.ToDateTime(valCurs.Date).ToUniversalTime()))
+                {
+                    db.Valutes.Where(x => x.Id == newValute.Id).FirstOrDefault().DateValues.Add(new DateValue()
+                    {
+                        Date = Convert.ToDateTime(valCurs.Date).ToUniversalTime(),
+                        Nominal = val.Nominal,
+                        Value = Convert.ToDouble(val.Value),
+                        VunitRate = Convert.ToDouble(val.VunitRate)
+                    });
+                    await db.SaveChangesAsync();
+                }
             }
         }
     }
