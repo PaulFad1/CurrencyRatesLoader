8b4545ad0a787e9e6a6b283358fc5f13df40f8ec
PaulFad <paul_fad@icloud.com>
2023-10-31 12:48:48 +0300

    Add migration

---
+++ Migrations/20231031093300_first.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace CurrencyRatesLoader.Migrations
+{
+    /// <inheritdoc />
+    public partial class first : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Valutes",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    NumCode = table.Column<int>(type: "integer", nullable: false),
+                    CharCode = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Valutes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DateValue",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Nominal = table.Column<long>(type: "bigint", nullable: false),
+                    Value = table.Column<double>(type: "double precision", nullable: false),
+                    VunitRate = table.Column<double>(type: "double precision", nullable: false),
+                    ValuteId = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DateValue", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DateValue_Valutes_ValuteId",
+                        column: x => x.ValuteId,
+                        principalTable: "Valutes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DateValue_ValuteId",
+                table: "DateValue",
+                column: "ValuteId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DateValue");
+
+            migrationBuilder.DropTable(
+                name: "Valutes");
+        }
+    }
+}
